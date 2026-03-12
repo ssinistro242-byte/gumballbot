84e167446df0d38be413b437d8070c7d81146840
ssinistro242-byte <ssinistro242@gmail.com>
2026-03-12 17:26:45 -0300

    Implement audio download feature for !play command

--- Comandos/Download.js
+++ Comandos/Download.js
@@ -1,1 +1,40 @@
+const yts = require("yt-search")
+const ytdl = require("ytdl-core")
+const fs = require("fs")
 
+module.exports = {
+
+name: "!play",
+
+async execute(sock,msg,text){
+
+const from = msg.key.remoteJid
+
+const query = text.replace("!play ","")
+
+const search = await yts(query)
+
+const video = search.videos[0]
+
+sock.sendMessage(from,{text:"baixando "+video.title})
+
+const stream = ytdl(video.url,{filter:"audioonly"})
+
+const path = "./music.mp3"
+
+stream.pipe(fs.createWriteStream(path))
+
+stream.on("end", async()=>{
+
+await sock.sendMessage(from,{
+audio: fs.readFileSync(path),
+mimetype:"audio/mp4"
+})
+
+fs.unlinkSync(path)
+
+})
+
+}
+
+}
