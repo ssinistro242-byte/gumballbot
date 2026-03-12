d518fb0414a37067cbb53c884cc430fe616b0322
ssinistro242-byte <ssinistro242@gmail.com>
2026-03-12 17:25:52 -0300

    Add dice roll command to Diversao.js

--- Comandos/Diversao.js
+++ Comandos/Diversao.js
@@ -1,1 +1,15 @@
+module.exports = {
 
+name: "!dado",
+
+async execute(sock,msg){
+
+const from = msg.key.remoteJid
+
+const n = Math.floor(Math.random()*6)+1
+
+sock.sendMessage(from,{text:"🎲 "+n})
+
+}
+
+}
